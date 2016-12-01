097c16ad48cdcd86991a3e0634cc436863d5bf31
Punknoodles <caseypinit@gmail.com>
2016-12-01 06:59:52 -0800

    moved to a class (test); added: modes (p|r), ping

--- lib/regamed.rb
+++ lib/regamed.rb
@@ -1,7 +1,27 @@
-def hello
-  return "Hello World"
-end
+class test
+  def initialize(mode="r")
+    if mode == "r" or mode == "p"
+      @mode = mode
+    else
+      puts "ERROR::Could not understand mode passed to test class\nUse r -> return the answer | p -> puts and return the answer"
+    end
+    
+  def ping
+    if @mode == "p"
+      puts "pong"
+    end
+    return "pong"
+  
+  def hello
+    if @mode == "p"
+      puts "Hello World!"
+    end
+    return "Hello World!"
+  end
 
-def echo(phrase)
-  return phrase
+  def echo(phrase)
+    if @mode == "p"
+      puts "Hello World!"
+    return phrase
+  end
 end
